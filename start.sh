3890293aefd811d084900787c76adb9daf0b68bc
Gavin <gavin.cui@mail.utoronto.ca>
2020-06-03 01:04:31 -0400

    test

--- start.sh
+++ start.sh
@@ -14,27 +14,29 @@ fi
 if [ -f "already_ran" ]; then
     echo "Already ran the Entrypoint once. Holding indefinitely for debugging."
     cat
+else
+    # always copy config.yaml and target.py from shared folder if exist
+    TARGET_FILE=/shared/target.py
+    TARGET_DEST=/rsyscall_fuzzer/controller/rscfuzzer/
+    if test -f "$TARGET_FILE"; then
+        cp $TARGET_FILE $TARGET_DEST
+        echo "$TARGET_FILE updated"
+    else
+        echo "$TARGET_FILE dose not exist, use the default one"
+    fi
+
+    cd /rsyscall_fuzzer/controller/
+    echo $#
+    if [ $# -eq 0 ]
+    then
+        echo "No arguments supplied, run test..."
+        /rsyscall_fuzzer/controller/main.py -c /test_config.yaml -d -e docker_test
+        exit 0
+    else
+        /rsyscall_fuzzer/controller/main.py $@
+    fi
 fi
 touch already_ran
 
-# always copy config.yaml and target.py from shared folder if exist
-TARGET_FILE=/shared/target.py
-TARGET_DEST=/rsyscall_fuzzer/controller/rscfuzzer/
-if test -f "$TARGET_FILE"; then
-    cp $TARGET_FILE $TARGET_DEST
-    echo "$TARGET_FILE updated"
-else
-    echo "$TARGET_FILE dose not exist, use the default one"
-fi
 
-cd /rsyscall_fuzzer/controller/
-echo $#
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied, run test..."
-    /rsyscall_fuzzer/controller/main.py -c /test_config.yaml -d -e docker_test
-    exit 0
-else
-    /rsyscall_fuzzer/controller/main.py $@
-fi
 
