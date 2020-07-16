882c435d626a2372a5bbe7ebb6987b5468b0e4ee
Gavin <gavincrz@gmail.com>
2020-07-16 16:07:42 -0400

    add zlib

---
+++ zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/gavin/zlib/adler32.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/adler32.o"
+  "/home/gavin/zlib/compress.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/compress.o"
+  "/home/gavin/zlib/crc32.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/crc32.o"
+  "/home/gavin/zlib/deflate.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/deflate.o"
+  "/home/gavin/zlib/gzclose.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/gzclose.o"
+  "/home/gavin/zlib/gzlib.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/gzlib.o"
+  "/home/gavin/zlib/gzread.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/gzread.o"
+  "/home/gavin/zlib/gzwrite.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/gzwrite.o"
+  "/home/gavin/zlib/infback.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/infback.o"
+  "/home/gavin/zlib/inffast.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/inffast.o"
+  "/home/gavin/zlib/inflate.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/inflate.o"
+  "/home/gavin/zlib/inftrees.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/inftrees.o"
+  "/home/gavin/zlib/trees.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/trees.o"
+  "/home/gavin/zlib/uncompr.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/uncompr.o"
+  "/home/gavin/zlib/zutil.c" "/home/gavin/zlib/cmake-build-debug/CMakeFiles/zlibstatic.dir/zutil.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_LARGEFILE64_SOURCE=1"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ zlib/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/gavin/zlib
+# Build directory: /home/gavin/zlib/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+add_test(example64 "example64")
